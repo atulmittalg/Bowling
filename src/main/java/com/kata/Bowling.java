1b6f4d919cd4d9a6c2ca8b0cb0ef1b14a0c0102a
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-02-24 16:37:12 +0100

    Refactor: extracting method to calculate roll Score when Strike

--- src/main/java/com/kata/Bowling.java
+++ src/main/java/com/kata/Bowling.java
@@ -6,23 +6,29 @@ public class Bowling {
     private int totalNumberOfRolls;
 
     public int calculateGameScore() {
-        for(int rollPointer = 0; rollPointer < totalNumberOfRolls; rollPointer++){
-            int rollScore = listOfRollScores[rollPointer];
+        for(int rollPosition = 0; rollPosition < totalNumberOfRolls; rollPosition++){
+            int rollScore = listOfRollScores[rollPosition];
             gameScore+=rollScore;
-            if(isStrike(rollScore)){
-                if(isStrike(listOfRollScores[rollPointer+2])) gameScore = gameScore + listOfRollScores[rollPointer+2] + listOfRollScores[rollPointer+4];
-                else  gameScore = gameScore + listOfRollScores[rollPointer+2] + listOfRollScores[rollPointer+3];
-            }
+            calculateStrikeRollScore(rollPosition, rollScore);
         }
         return gameScore;
     }
 
-    public void calculateScorePerRoll(final int pinsDropped){
-        listOfRollScores[totalNumberOfRolls++] = pinsDropped;
-        if(isStrike(pinsDropped))   totalNumberOfRolls++;
+    private void calculateStrikeRollScore(int rollPosition, int rollScore) {
+        if(isAStrike(rollScore)){
+            int rollScoreAfterStrike = listOfRollScores[rollPosition+2];
+            int frameScore = rollScoreAfterStrike + listOfRollScores[rollPosition+3];
+            if(isAStrike(rollScoreAfterStrike)) gameScore = gameScore + rollScoreAfterStrike + listOfRollScores[rollPosition+4];
+            else  gameScore = gameScore + frameScore;
+        }
     }
 
-    private boolean isStrike(final int pinsDropped){
+    public void calculateScorePerRoll(final int pinsDropped){
+        listOfRollScores[totalNumberOfRolls++] = pinsDropped;
+        if(isAStrike(pinsDropped))   totalNumberOfRolls++;
+    }
+
+    private boolean isAStrike(final int pinsDropped){
         return pinsDropped == 10;
     }
 }
