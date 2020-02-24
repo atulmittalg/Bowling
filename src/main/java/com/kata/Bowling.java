24f5c18fd20c99a9de4609a3da36053cd52bedb9
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-02-24 16:13:09 +0100

    calculating score when a Strike is followed by 2 normal rolls

--- src/main/java/com/kata/Bowling.java
+++ src/main/java/com/kata/Bowling.java
@@ -2,12 +2,21 @@ package main.java.com.kata;
 
 public class Bowling {
     private int gameScore;
+    private int[] listOfRollScores = new int[22];
+    private int totalNumberOfRolls;
 
     public int calculateGameScore() {
+        for(int rollPointer = 0; rollPointer < totalNumberOfRolls; rollPointer++){
+            gameScore+=listOfRollScores[rollPointer];
+            if(listOfRollScores[rollPointer] == 10){
+                gameScore = gameScore + listOfRollScores[rollPointer+2] + listOfRollScores[rollPointer+3];
+            }
+        }
         return gameScore;
     }
 
     public void calculateScorePerRoll(int pinsDropped){
-        gameScore += pinsDropped;
+        listOfRollScores[totalNumberOfRolls++] = pinsDropped;
+        if(pinsDropped == 10)   totalNumberOfRolls++;
     }
 }
--- src/test/java/com/kata/BowlingTest.java
+++ src/test/java/com/kata/BowlingTest.java
@@ -14,9 +14,9 @@ public class BowlingTest {
         bowlingGame = new Bowling();
     }
 
-    private void setRollScoreTo5ForMultipleRolls(final int numberOfRolls){
+    private void setRollScoreTo5ForMultipleRolls(final int numberOfRolls, final int pinsDropped){
         for(int i =0; i < numberOfRolls; i++){
-            bowlingGame.calculateScorePerRoll(5);
+            bowlingGame.calculateScorePerRoll(pinsDropped);
         }
     }
 
@@ -33,13 +33,20 @@ public class BowlingTest {
 
     @Test
     public void shouldSetScoreTo5When5PinsAreDroppedForSingleRoll(){
-        setRollScoreTo5ForMultipleRolls(1);
+        setRollScoreTo5ForMultipleRolls(1,5);
         Assert.assertEquals(5,bowlingGame.calculateGameScore());
     }
 
     @Test
     public void shouldSetScoreTo10When5PinsAreDroppedTwice(){
-        setRollScoreTo5ForMultipleRolls(2);
+        setRollScoreTo5ForMultipleRolls(2,5);
         Assert.assertEquals(10,bowlingGame.calculateGameScore());
+    }
+
+    @Test
+    public void shouldCalculateScoreWhenGettingStrikeAnd2NormalRolls(){
+        bowlingGame.calculateScorePerRoll(10);
+        setRollScoreTo5ForMultipleRolls(2,3);
+        Assert.assertEquals(22,bowlingGame.calculateGameScore());
     }
 }
